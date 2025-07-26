1864fdf2f72608057712280da05d1f6cc002a6eb
Nirmala Kadali <99960001+Nirmalakadali@users.noreply.github.com>
2025-07-26 11:33:39 +0530

    Update index.cjs

--- project/server/index.cjs
+++ project/server/index.cjs
@@ -320,7 +320,7 @@ const path = require("path");
 // ✅ Serve React build
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
